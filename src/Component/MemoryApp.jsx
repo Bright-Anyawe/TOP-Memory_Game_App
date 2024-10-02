687ade4f3679ba22691edffdecf61e74f317ec13
Bright-Anyawe <157108952+Bright-Anyawe@users.noreply.github.com>
2024-10-02 20:13:30 +0000

    Refactor: display card on the page
    This allow user to see and interact with the cards

--- src/Component/MemoryApp.jsx
+++ src/Component/MemoryApp.jsx
@@ -2,9 +2,19 @@ import Card from "./Cards";
 
 export default function MemoryApp() {
   return (
-    <div>
-      <h1>Avengers Memory Game</h1>
-      <Card />
-    </div>
+    <main>
+      <header>
+        <h1>Avengers Memory Game</h1>
+        <h3>
+          Improve your score by clicking on an image <span>Note</span>!
+          Don&apos;t click on an image more than once
+        </h3>
+      </header>
+
+
+      <section className="cardsContainer">
+        <Card />
+      </section>
+    </main>
   );
 }
